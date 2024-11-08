8f612de066b91ec60c911995df457fd75e0c3f96
Jaeyoung <jyko1101@gmail.com>
2024-11-08 15:16:12 +0900

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,30 @@ import AnimatedBox from './components/AnimatedBox';
 import logo from './assets/logo.svg';
 import Character from './components/Character/Character';
 import Button from './components/Button/Button';
+import MiningAnimation from './components/MiningAnimation/MiningAnimation';
+import Pickaxe from './components/Pickaxe/Pickaxe';
+let cnt = 0;
 
 function App() {
-
-  const handleClick = () => { alert('Button clicked!'); };
+  const handleClick = () => { alert('Button clicked!'); cnt++; console.log("cnt: " + cnt);};
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-title">
-          <p> 아르테타를 따라하라! </p>
+          <h1> Mining Clicker {cnt}</h1>
+          <MiningAnimation/>
         </div>
         <div>
           <Button className="btn-start" label="Start Game" onClick={handleClick} />
         </div>
 
+        <div>
+          <p>하이</p>
+          <h1> Mining Anim </h1>
+          <Pickaxe/>
+        </div>
 
-        {/* <div> <AnimatedBox /></div>
-
-        <div> <Character name="tmpman" /></div> */}
 
       </header>
     </div>
