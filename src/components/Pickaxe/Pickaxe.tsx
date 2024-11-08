b310161dd92013fb99dbc67f7a02537a295f33be
Jaeyoung <jyko1101@gmail.com>
2024-11-08 15:49:11 +0900

    feat: anim on pickaxe

--- src/components/Pickaxe/Pickaxe.tsx
+++ src/components/Pickaxe/Pickaxe.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import pickaxeImg from '../../assets/pickaxe.png';
 import './Pickaxe.css';
+import { useSpring, animated } from '@react-spring/web';
+
 const Pickaxe: React.FC = () => {
+
+  const props = useSpring({
+    from: { transform: 'rotate(-45deg)' },
+    to: async (next) => {
+      await next({ transform: 'rotate(-225deg)', config: { tension: 200, friction: 10 }});
+      await next({ transform: 'rotate(-45deg)', config: {duration: 1500} });
+    },
+    loop: true,
+  });
   return (
     <div className="pickaxe-img">
-      <img className="pickaxe-resized" src={pickaxeImg} alt="pickaxe" />
+      <animated.img className="pickaxe-resized" src={pickaxeImg} alt="pickaxe" style={props} />
     </div>
   );
 };
